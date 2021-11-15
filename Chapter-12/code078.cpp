b4aa3f6d03422e3da7381940fab7f31abbac9435
IRVING-L <642924368@qq.com>
2021-11-16 00:25:28 +0800

    2021年11月16日00:25:23

--- Chapter-12/code078.cpp
+++ Chapter-12/code078.cpp
@@ -20,7 +20,7 @@ struct ListNode
 class Solution
 {
 private:
-    ListNode *mergeTwoList(ListNode *l1,ListNode *l2)
+    ListNode *mergeTwoList(ListNode *l1, ListNode *l2)
     {
         //创建一个新的链表节点
         ListNode *newList = new ListNode(0);
@@ -30,15 +30,14 @@ private:
         ListNode *curr1 = l1;
         ListNode *curr2 = l2;
         //遍历这两个链表
-        while(curr1 && curr2)
+        while (curr1 && curr2)
         {
-            if(curr1->val < curr2->val)
+            if (curr1->val < curr2->val)
             {
                 newList->next = curr1;
                 //向前移动指针
                 curr1 = curr1->next;
                 newList = newList->next;
-                
             }
             else
             {
@@ -47,11 +46,11 @@ private:
                 newList = newList->next;
             }
         }
-        if(curr1)
+        if (curr1)
         {
             newList->next = curr1;
         }
-        if(curr2)
+        if (curr2)
         {
             newList->next = curr2;
         }
@@ -60,17 +59,28 @@ private:
     ListNode *mergeSortLists(vector<ListNode *> &lists, int left, int right)
     {
         //首先，需要明确的是，这个函数是一个递归
-
-        //我们从容器中，分别取出，要排序的两个链表
-        ListNode *l = lists[left];
-        ListNode *r = lists[right];
-
-        //调用函数，合并两个有序链表
-        ListNode *sortedList = mergeTwoList(l,r);
+        //我们把可以把容器分为两个区间
+        //首先，我们先判断一下这个区间的大小，是否足以支撑我们完成后面的操作
+        if (left == right)
+        {
+            //区间长度等于1
+            return lists[left];
+        }
+        //如果left大于right了，怎么处理
+        if(left > right)
+        {
+            return lists[right];
+        }
+        int mid = (left + right) >> 1;
+        ListNode *sortedList1 = mergeSortLists(lists, left, mid);
+        ListNode *sortedList2 = mergeSortLists(lists, mid + 1, right);//mid+1有可能会造成越界
+        ListNode *mergedAndSortedList = mergeTwoList(sortedList1, sortedList2);
+        return mergedAndSortedList;
     }
+
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists)
     {
-        
+        return mergeSortLists(lists, 0, lists.size());
     }
 };
